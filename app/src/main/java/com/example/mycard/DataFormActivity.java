a5db62d3e84f935521575d23741a2681a12114aa
Kenny <kenny.rakotoarimanana@gmail.com>
2021-04-30 00:07:52 +0300

    My card

---
+++ app/src/main/java/com/example/mycard/DataFormActivity.java
@@ -0,0 +1,347 @@
+        package com.example.mycard;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.PendingIntent;
+import android.content.ContentProviderOperation;
+import android.content.ContentProviderResult;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
+import android.nfc.tech.NdefFormatable;
+import android.nfc.tech.NfcA;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.provider.ContactsContract;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.webkit.PermissionRequest;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONStringer;
+
+import java.util.ArrayList;
+
+        public class DataFormActivity extends AppCompatActivity {
+    public static final String Error_Detected = "No NFC Tag Detected";
+    public static final String Write_Success = "Information written successsfully";
+    public static final String Write_Error = "Error during writing, try again";
+    NfcAdapter nfcAdapter;
+    PendingIntent pendingIntent;
+    IntentFilter[] writingTagFilters;
+    boolean writeMode;
+    Tag myTag;
+    Context context;
+    TextView firstname;
+    TextView lastname;
+    TextView phone;
+    TextView address;
+    TextView email;
+    Button sendInformation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_form);
+        firstname = (TextView) findViewById(R.id.first_name);
+        lastname = (TextView) findViewById(R.id.last_name);
+        phone = (TextView) findViewById(R.id.Phone);
+        address = (TextView) findViewById(R.id.address);
+        email = (TextView) findViewById(R.id.email);
+        sendInformation = findViewById(R.id.button);
+        context = this;
+        sendInformation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    if (myTag == null) {
+                        Toast.makeText(context, Error_Detected, Toast.LENGTH_LONG).show();
+                    } else {
+                        JSONObject informations = new JSONObject();
+                        informations.put("firstname", firstname.getText());
+                        informations.put("lastname", lastname.getText());
+                        informations.put("phone", phone.getText());
+                        informations.put("address", address.getText());
+                        informations.put("email", email.getText());
+
+                        write("" + informations.toString(), myTag);
+                        Toast.makeText(context, Write_Success, Toast.LENGTH_LONG).show();
+                    }
+                } catch (Exception e) {
+                    Toast.makeText(context, e.getMessage(), Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+            }
+        });
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (nfcAdapter == null) {
+            Toast.makeText(context, "This device does not support NFC", Toast.LENGTH_LONG).show();
+            finish();
+        }
+        try {
+            readFromIntent(getIntent());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+        tagDetected.addCategory(Intent.CATEGORY_DEFAULT);
+        writingTagFilters = new IntentFilter[] {tagDetected};
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+        try {
+            readFromIntent(intent);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction())) {
+            myTag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        writeModeOff();
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        writeModeOn();
+    }
+
+    private void writeModeOn() {
+        writeMode = true;
+        if(nfcAdapter==null)
+        {
+            nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        }
+        nfcAdapter.enableForegroundDispatch(this, pendingIntent, writingTagFilters, null);
+    }
+
+    private void writeModeOff() {
+        writeMode = false;
+        if(nfcAdapter==null)
+        {
+            nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        }
+        nfcAdapter.disableForegroundDispatch(this);
+    }
+
+    private void readFromIntent(Intent intent) throws JSONException {
+        String action = intent.getAction();
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)
+            || NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)
+            || NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
+            Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            NdefMessage[] msgs = null;
+            if (rawMsgs != null) {
+                msgs = new NdefMessage[rawMsgs.length];
+                for (int i = 0; i < rawMsgs.length; i++) {
+                    msgs[i] = (NdefMessage) rawMsgs[i];
+                }
+            }
+            buildTagViews(msgs);
+        }
+    }
+
+    private void buildTagViews(NdefMessage[] msgs) throws JSONException {
+        if (msgs == null || msgs.length == 0) return;
+
+        String text = "";
+        byte[] payload = msgs[0].getRecords()[0].getPayload();
+        String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16";
+        int languageCodeLength = payload[0] & 0063;
+
+        try {
+            text = new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
+        } catch (Exception e) {
+            Log.e("UnsupportedEncoding", e.toString());
+        }
+        JSONObject contact = new JSONObject(text);
+        addContact(contact.getString("firstname"), contact.getString("lastname"), contact.getString("phone"), contact.getString("address"), contact.getString("email"));
+    }
+
+    private void write(String text, Tag tag) throws Exception {
+        NdefRecord[] records = { createRecord(text) };
+        NdefMessage message = new NdefMessage(records);
+        Ndef ndef = Ndef.get(tag);
+        if (ndef != null) {
+            ndef.connect();
+            ndef.writeNdefMessage(message);
+            ndef.close();
+        } else {
+            NdefFormatable ndefFormatable = NdefFormatable.get(tag);
+            if (ndefFormatable != null) {
+                // initialize tag with new NDEF message
+                try {
+                    ndefFormatable.connect();
+                    ndefFormatable.format(message);
+                } finally {
+                    try {
+                        ndefFormatable.close();
+                    } catch (Exception e) {
+                    }
+                }
+            }
+        }
+    }
+
+    private NdefRecord createRecord(String text) throws Exception {
+        String lang = "en";
+        byte[] textBytes = text.getBytes();
+        byte[] langBytes = lang.getBytes("US-ASCII");
+        int langLength = langBytes.length;
+        int textLength = textBytes.length;
+        byte[] payload = new byte[1 + langLength + textLength];
+
+        payload[0] = (byte) langLength;
+
+        System.arraycopy(langBytes, 0, payload, 1, langLength);
+        System.arraycopy(textBytes, 0, payload, 1 + langLength, textLength);
+
+        NdefRecord recordNFC = new NdefRecord(NdefRecord.TNF_WELL_KNOWN, NdefRecord.RTD_TEXT, new byte[0], payload);
+
+        return recordNFC;
+    }
+
+            private void contactPermission() {
+                boolean permissionOperation;
+                if (android.os.Build.VERSION.SDK_INT >= 23) {
+                    // only for gingerbread and newer versions
+                    String permission = Manifest.permission.WRITE_CONTACTS;
+                    if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(DataFormActivity.this, new String[]{Manifest.permission.WRITE_CONTACTS, Manifest.permission.READ_CONTACTS}, 100);
+                    } else {
+                        permissionOperation = true;
+                    }
+                } else {
+                    permissionOperation = true;
+                }
+            }
+
+            @Override
+            public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+                switch (requestCode) {
+                    case 100: {
+                        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                            // Add your function here which open camera
+                        } else {
+                            boolean somePermissionsForeverDenied = false;
+                            for(String permission: permissions){
+                                if(ActivityCompat.shouldShowRequestPermissionRationale(this, permission)){
+                                    //denied
+                                    Log.e("denied", permission);
+                                }else{
+                                    if(ActivityCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED){
+                                        //allowed
+                                        Log.e("allowed", permission);
+                                    } else{
+                                        //set to never ask again
+                                        Log.e("set to never ask again", permission);
+                                        somePermissionsForeverDenied = true;
+                                    }
+                                }
+                            }
+                            if(somePermissionsForeverDenied){
+                                final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+                                alertDialogBuilder.setTitle("Permissions Required")
+                                        .setMessage("You have forcefully denied some of the required permissions " +
+                                                "for this action. Please open settings, go to permissions and allow them.")
+                                        .setPositiveButton("Settings", new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                                                        Uri.fromParts("package", getPackageName(), null));
+                                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                                startActivity(intent);
+                                            }
+                                        })
+                                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                            }
+                                        })
+                                        .setCancelable(false)
+                                        .create()
+                                        .show();
+                            }
+                            Toast.makeText(getApplication(), "Permission required", Toast.LENGTH_LONG).show();
+                        }
+                        return;
+                    }
+                }
+            }
+
+            private void addContact(String given_name, String name, String mobile, String home, String email) {
+                contactPermission();
+                ArrayList<ContentProviderOperation> contact = new ArrayList<ContentProviderOperation>();
+                contact.add(ContentProviderOperation.newInsert(ContactsContract.RawContacts.CONTENT_URI)
+                        .withValue(ContactsContract.RawContacts.ACCOUNT_TYPE, null)
+                        .withValue(ContactsContract.RawContacts.ACCOUNT_NAME, null)
+                        .build());
+
+                // first and last names
+                contact.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                        .withValueBackReference(ContactsContract.RawContacts.Data.RAW_CONTACT_ID, 0)
+                        .withValue(ContactsContract.RawContacts.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE)
+                        .withValue(ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME, given_name)
+                        .withValue(ContactsContract.CommonDataKinds.StructuredName.FAMILY_NAME, name)
+                        .build());
+
+                // Contact No Mobile
+                contact.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                        .withValueBackReference(ContactsContract.RawContacts.Data.RAW_CONTACT_ID, 0)
+                        .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
+                        .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, mobile)
+                        .withValue(ContactsContract.CommonDataKinds.Phone.TYPE, ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE)
+                        .build());
+
+                // Contact Home
+                contact.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                        .withValueBackReference(ContactsContract.RawContacts.Data.RAW_CONTACT_ID, 0)
+                        .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
+                        .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, home)
+                        .withValue(ContactsContract.CommonDataKinds.Phone.TYPE, ContactsContract.CommonDataKinds.Phone.TYPE_HOME)
+                        .build());
+
+                // Email    `
+                contact.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                        .withValueBackReference(ContactsContract.RawContacts.Data.RAW_CONTACT_ID, 0)
+                        .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE)
+                        .withValue(ContactsContract.CommonDataKinds.Email.DATA, email)
+                        .withValue(ContactsContract.CommonDataKinds.Email.TYPE, ContactsContract.CommonDataKinds.Email.TYPE_WORK)
+                        .build());
+
+                try {
+                    ContentProviderResult[] results = getContentResolver().applyBatch(ContactsContract.AUTHORITY, contact);
+                    Toast.makeText(context, "Contact added", Toast.LENGTH_LONG).show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+}
